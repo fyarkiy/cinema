e92a68f48afb85aa07246268290c7841ee4fa2e2
romandubovskyi <rdubowskij@gmail.com>
2020-10-22 15:54:15 +0300

    another mock commit

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -22,6 +22,7 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
 
 
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
